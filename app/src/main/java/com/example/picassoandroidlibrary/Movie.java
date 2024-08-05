18215c576c9277d80c6cd6a7be5924a30e514011
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-08-05 10:16:15 +0330

    first commit

---
+++ app/src/main/java/com/example/picassoandroidlibrary/Movie.java
@@ -0,0 +1,29 @@
+package com.example.picassoandroidlibrary;
+
+public class Movie {
+    private String name;
+    private boolean isFavorite = false;
+    private String imageUrl;
+
+    public Movie(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean getIsFavorite() {
+        return isFavorite;
+    }
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+
+    public void toggleFavorite() {
+        isFavorite = !isFavorite;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
